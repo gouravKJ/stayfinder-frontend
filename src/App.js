8a17e97d46f50352fe07c0c3446ab80a8ad2339d
gouravKJ <gouravjaiswal2006@gmail.com>
2025-06-22 00:11:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ListingDetail from "./pages/ListingDetail";
+import UserBookings from "./pages/UserBookings";
+import AdminPanel from "./pages/Adminpanel"; 
+import AdminLogin  from "./pages/Adminlogin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/listing/:id" element={<ListingDetail />} />
+          <Route path="/bookings" element={<UserBookings />} />
+            <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/admin-login" element={<AdminLogin/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
